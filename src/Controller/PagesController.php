7fefcb0953d72e4bef94994c55ba39669c1af276
Charles Marcoin <charles.marcoin@gmail.com>
2020-06-10 22:55:37 +0200

    basic homepage view

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PagesController
@@ -9,11 +10,12 @@ class PagesController
     /**
      * Page d'accueil site upt golf
      * 
-     * @Route()
+     * @Route("/", name="homepage")
      *
      * @return void
      */
     public function home()
     {
+        return new Response("Bienvenue sur le site de l'upt golf");
     }
 }
