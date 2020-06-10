f5ae726c7dbc46b65b9c891308d513ee8277a632
Charles Marcoin <charles.marcoin@gmail.com>
2020-06-10 22:51:06 +0200

    added page controller

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController
+{
+    /**
+     * Page d'accueil site upt golf
+     * 
+     * @Route()
+     *
+     * @return void
+     */
+    public function home()
+    {
+    }
+}
