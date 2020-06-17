a12754b734edc08bfdeed754e44a2362769a37b6
Charles Marcoin <charles.marcoin@gmail.com>
2020-06-17 11:48:08 +0200

    created first fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Golf;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        // Nouvelle factory faker en français
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 30; $i++) {
+            $golf = new Golf();
+
+            $name = $faker->sentence();
+            $contact = $faker->name();
+            $email = $faker->email();
+            $comments = $faker->realText($maxNbChars = 250, $indexSize = 2);
+            $address = $faker->Address();
+            $websiteUrl = $faker->domainName();
+            $phoneNumber = $faker->phoneNumber();
+
+            $golf->setName($name)
+                ->setPhoneNumber($phoneNumber)
+                ->setWebsiteUrl($websiteUrl)
+                ->setAddress($address)
+                ->setComments($comments)
+                ->setEmail($email)
+                ->setContact($contact);
+
+            $manager->persist($golf);
+        }
         $manager->flush();
     }
 }
